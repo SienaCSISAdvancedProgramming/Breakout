19662d702c0d7ce320ec4dbed6fda96de65b93a3
jteresco <terescoj@gmail.com>
2022-04-09 13:18:51 -0400

    Formatting.

--- BreakoutPaddle.java
+++ BreakoutPaddle.java
@@ -5,13 +5,13 @@ import java.awt.event.MouseEvent;
 import javax.swing.JComponent;
 
 /**
-   Implementation for the paddle for our breakout game.
-
-   @author Jim Teresco
-   @version Spring 2020
-*/
+ * Implementation for the paddle for our breakout game.
+ * 
+ * @author Jim Teresco
+ * @version Spring 2022
+ */
 class BreakoutPaddle extends MouseAdapter {
-    
+
     // dimensions of the paddle
     public static final int PADDLE_WIDTH = 50;
     public static final int PADDLE_HEIGHT = 20;
@@ -23,60 +23,59 @@ class BreakoutPaddle extends MouseAdapter {
     private JComponent component;
 
     /**
-       Construct the paddle, and add it as a mouse motion listener
-       on the JComponent in which it lives.
-
-       @param c the JComponent in which we will be drawing
-    */
+     * Construct the paddle, and add it as a mouse motion listener
+     * on the JComponent in which it lives.
+     * 
+     * @param c the JComponent in which we will be drawing
+     */
     public BreakoutPaddle(JComponent c) {
 
-	component = c;
-	component.addMouseMotionListener(this);
+        component = c;
+        component.addMouseMotionListener(this);
     }
-    
-    /**
-       Draw the paddle at its current position
 
-       @param g the Graphics object in which to draw
-    */
+    /**
+     * Draw the paddle at its current position
+     * 
+     * @param g the Graphics object in which to draw
+     */
     protected void paint(Graphics g) {
 
-	// paddle at its current position
-	g.setColor(Color.black);
-	g.fillRect(paddleX, component.getHeight() - 2*PADDLE_HEIGHT,
-		   PADDLE_WIDTH, PADDLE_HEIGHT);
+        // paddle at its current position
+        g.setColor(Color.black);
+        g.fillRect(paddleX, component.getHeight() - 2 * PADDLE_HEIGHT,
+                PADDLE_WIDTH, PADDLE_HEIGHT);
     }
 
     /**
-       Mouse move event handler to control the paddle
-       
-       @param e mouse event info
-    */
+     * Mouse move event handler to control the paddle
+     * 
+     * @param e mouse event info
+     */
     @Override
     public void mouseMoved(MouseEvent e) {
 
-	paddleX = e.getPoint().x;
-	if (paddleX > component.getWidth() - PADDLE_WIDTH) {
-	    paddleX = component.getWidth() - PADDLE_WIDTH;
-	}
+        paddleX = e.getPoint().x;
+        if (paddleX > component.getWidth() - PADDLE_WIDTH) {
+            paddleX = component.getWidth() - PADDLE_WIDTH;
+        }
 
     }
 
     /**
-       Check if the paddle overlaps the ball, as defined by its center
-       and radius.
-
-       @param ballX x-coordinate of the center of the ball
-       @param ballY y-coordinate of the center of the ball
-       @param ballR radius of the ball
-       @return true if the paddle overlaps the bal 
-    */
+     * Check if the paddle overlaps the ball, as defined by its center
+     * and radius.
+     * 
+     * @param ballX x-coordinate of the center of the ball
+     * @param ballY y-coordinate of the center of the ball
+     * @param ballR radius of the ball
+     * @return true if the paddle overlaps the bal
+     */
     public boolean overlapsBall(int ballX, int ballY, int ballR) {
 
-	return Collision.circleOverlapsRectangle(ballX, ballY, ballR,
-						 paddleX,
-						 component.getHeight() - 2*PADDLE_HEIGHT,
-						 PADDLE_WIDTH, PADDLE_HEIGHT);
+        return Collision.circleOverlapsRectangle(ballX, ballY, ballR,
+                paddleX,
+                component.getHeight() - 2 * PADDLE_HEIGHT,
+                PADDLE_WIDTH, PADDLE_HEIGHT);
     }
 }
-
